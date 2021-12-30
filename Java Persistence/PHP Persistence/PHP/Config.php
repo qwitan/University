ddb64e0a8cdc6438df013e13b0301e56bd12d838
qwitan <75077123+qwitan@users.noreply.github.com>
2021-12-30 06:22:31 -0800

    Add files via upload

---
+++ Java Persistence/PHP Persistence/PHP/Config.php
@@ -0,0 +1,9 @@
+<?php
+// enable debugging
+ini_set('display_errors', 'On');
+// define constants for PDO connection
+define("DSN","mysql:dbname=user01_taxi;host=localhost");
+define("USER", "user01");
+define("PWD", "user01");
+
+?>
