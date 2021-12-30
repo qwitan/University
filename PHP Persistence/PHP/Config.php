1be2931d1a6f39176a707c0eea08b26f56c83fef
qwitan <75077123+qwitan@users.noreply.github.com>
2021-12-30 06:28:29 -0800

    Delete PHP Persistence/PHP directory

--- PHP Persistence/PHP/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// enable debugging
-ini_set('display_errors', 'On');
-// define constants for PDO connection
-define("DSN","mysql:dbname=user01_taxi;host=localhost");
-define("USER", "user01");
-define("PWD", "user01");
-
-?>
